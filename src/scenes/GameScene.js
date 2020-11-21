7ef5418d06aaba2cbe801c4859bdfb7c6cf3dbb2
Nicole Mastro <69874635+nicole026@users.noreply.github.com>
2020-11-21 11:51:46 -0500

    3 jewels no stars

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,6 +7,10 @@ let BLUE_KEY = 'blue';
 let YELLOW_KEY = 'yellow';
 let PLATFORM = 'platform';
 let PURPLE_PLAT = 'purpleplat';
+let GRAYPLAT = 'grayplat';
+// let GRAY_BAR = 'gray-bar';
+// let RED_KEY = 'red';
+let PURPLE_KEY = 'purple';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -22,6 +26,10 @@ export default class GameScene extends Phaser.Scene {
     this.load.image(YELLOW_KEY, '/assets/yellow_ball.png');
     this.load.image(PLATFORM, 'assets/strip1.png');
     this.load.image(PURPLE_PLAT, 'assets/purpleplatform.png');
+    this.load.image(GRAYPLAT, 'assets/grayplat.png');
+    // this.load.image(GRAY_BAR, 'assets/graybar.png');
+    // this.load.image(RED_KEY, 'assets/red_ball.png');
+    this.load.image(PURPLE_KEY, 'assets/purple_ball.png');
 
     this.load.spritesheet(DUDE_KEY, 'assets/dude.png', {
       frameWidth: 32,
@@ -32,31 +40,37 @@ export default class GameScene extends Phaser.Scene {
   create() {
     // this.add.image(400, 300, this.sky);
     this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor(
-      //   '#ffffff'
-      '#7851CC'
+      '#E1EAEC'
+      //   '#7851CC'
     );
 
     const platforms = this.createPlatforms();
     this.player = this.createPlayer();
-    const stars = this.createStars();
-    const blue = this.physics.add.image(550, 400, BLUE_KEY);
+    // const stars = this.createStars();
+    const blue = this.physics.add.image(150, 200, BLUE_KEY);
     const yellow = this.physics.add.staticGroup().create(10, 310, YELLOW_KEY);
+    // const graybar = this.physics.add.staticGroup().create(740, 480, GRAY_BAR);
     // const yellow = this.physics.add.image(590, 400, YELLOW_KEY);
+    // const red = this.physics.add.image(770, 400, RED_KEY);
+    const purple = this.physics.add.image(500, 400, PURPLE_KEY);
 
     this.physics.add.collider(this.player, platforms);
-    this.physics.add.collider(stars, platforms);
+    // this.physics.add.collider(stars, platforms);
     this.physics.add.collider(blue, platforms);
     this.physics.add.collider(yellow, platforms);
+    this.physics.add.collider(purple, platforms);
+    // this.physics.add.collider(red, platforms);
+    // this.physics.add.collider(this.player, graybar);
 
-    this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
+    console.log('this.physics -->', this.physics.add);
+
+    // this.physics.add.overlap(this.player, stars, this.collectStar, null, this);
+    // blue
     this.physics.add.overlap(this.player, blue, this.collectBlue, null, this);
-    this.physics.add.overlap(
-      this.player,
-      yellow,
-      this.collectYellow,
-      null,
-      this
-    );
+    //prettier-ignore
+    this.physics.add.overlap(this.player, yellow, this.collectYellow, null, this);
+    //prettier-ignore
+    this.physics.add.overlap(this.player, purple, this.collectPurple, null, this);
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -83,7 +97,7 @@ export default class GameScene extends Phaser.Scene {
   createPlatforms() {
     let platforms = this.physics.add.staticGroup();
     platforms.create(400, 568, PLATFORM).setScale(2).refreshBody();
-    platforms.create(600, 400, PLATFORM);
+    // platforms.create(600, 400, PLATFORM);
     platforms.create(50, 250, PLATFORM);
     // platforms.create(0, 380, PURPLE_PLAT);
 
@@ -120,29 +134,29 @@ export default class GameScene extends Phaser.Scene {
     return player;
   }
 
-  createStars() {
-    const stars = this.physics.add.group({
-      key: STAR_KEY,
-      repeat: 11,
-      setXY: {
-        x: 12,
-        y: 0,
-        stepX: 70,
-      },
-    });
+  //   createStars() {
+  //     const stars = this.physics.add.group({
+  //       key: STAR_KEY,
+  //       repeat: 11,
+  //       setXY: {
+  //         x: 12,
+  //         y: 0,
+  //         stepX: 70,
+  //       },
+  //     });
 
-    stars.children.iterate((child) => {
-      //this is fine, eslint is just being a jerk
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.5));
-    });
+  //     stars.children.iterate((child) => {
+  //       //this is fine, eslint is just being a jerk
+  //       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.5));
+  //     });
 
-    return stars;
-  }
+  //     return stars;
+  //   }
 
-  collectStar(player, star) {
-    star.disableBody(true, true);
-    this.scoreLabel.add(10);
-  }
+  //   collectStar(player, star) {
+  //     star.disableBody(true, true);
+  //     this.scoreLabel.add(10);
+  //   }
 
   collectBlue(player, blue) {
     blue.disableBody(true, true);
@@ -158,6 +172,15 @@ export default class GameScene extends Phaser.Scene {
     this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor(
       '#F4C11D'
     );
+  }
+
+  collectPurple(player, purple) {
+    purple.disableBody(true, true);
+    this.cameras.main.backgroundColor = Phaser.Display.Color.HexStringToColor(
+      '#7851CC'
+    );
+    let grayplat = this.physics.add.staticGroup().create(600, 400, GRAYPLAT);
+    this.physics.add.collider(this.player, grayplat);
   }
 
   createScoreLabel(x, y, score) {
